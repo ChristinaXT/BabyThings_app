4eab4d59975890cfc1da838e3914378b7df253f7
Christina <40068707+ChristinaXT@users.noreply.github.com>
2019-03-13 22:19:30 -0400

    Update clothing_items.rb

--- app/controllers/clothing_items.rb
+++ app/controllers/clothing_items.rb
@@ -2,7 +2,7 @@ class ClothingItemsController < ApplicationController
 
   get '/clothing_items' do
       if logged_in?
-        @clothing_items = clothing_item.all
+        @clothing_items = Clothing_item.all
         erb :'clothing_items/items'
       else
         redirect "/login"
