1f0df6585bde99462db079a7c882d6470e163c55
Christina Williams <christinaxtwilliams@gmail.com>
2019-03-15 19:51:56 +0000

    updated login view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,18 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "babythings_secret"
   end
 
-  get '/' do
-    erb :index
+   get "/" do
+   erb :index
   end
 
   helpers do
-
     def logged_in?
-      !!current_user
-  	end
+     !!session[:user_id]
+    end
 
-  	def current_user
-  		@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  	end
-  end
+    def current_user
+     User.find(session[:user_id])
+    end
+   end
+   
 end
