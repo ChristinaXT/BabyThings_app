ccb2039e146a15e159cfba851e82e54089ea1128
Christina <40068707+ChristinaXT@users.noreply.github.com>
2019-03-18 19:16:21 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,48 +6,49 @@ class UsersController < ApplicationController
   end 
   
   get '/signup' do
-    if logged_in?
-			redirect '/things'
-		else
-			erb :'users/create_user'
-		end
+    if !logged_in?
+	erb :'users/create_user'
+    else
+	redirect '/things'		
+    end
   end
   
   post '/signup' do
-    if params[:username] != "" && params[:email] != "" && params[:password] != ""
-			@user = User.create(params)
-			session[:user_id] = @user.id
-			redirect '/things'
-		else
-			redirect '/signup'
-		end
+    if params.values.any? {|value| value == ""}
+      erb :'users/create_users', locals: {message: "Please try again"}
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/things'
+    end
   end
   
   get '/login' do
-    if logged_in?
-			redirect '/things'
-		else
-			erb :'users/login'
-		end
-  end
-
-  post '/login' do
-    @user = User.find_by(username: params[:username])
-		if @user && @user.authenticate(params[:password])
-			session[:user_id] = @user.id 
-			redirect '/things'
-		else
-			redirect '/signup'
-		end
-  end
-
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect "/login"
+    if !logged_in?
+      erb :'users/login'
     else
-      redirect "/"
+      redirect to '/things'
     end
   end
 
-end 
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+      if @user && @user.authenticate(params[:password])
+	session[:user_id] = user.id 
+	redirect '/things'
+      else
+	erb :'users/login', locals: {message: "Please try again"}
+    end
+  end
+
+  get '/logout' do
+    if session[:user_id] != nil
+      session.destroy
+      redirect to '/'
+    else
+      redirect to '/things'
+    end
+  end
+
+end
