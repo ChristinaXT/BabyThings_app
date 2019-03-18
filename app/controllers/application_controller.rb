3fe5442faf86dde0384c6edd04a26435babe3e99
Christina <40068707+ChristinaXT@users.noreply.github.com>
2019-03-18 17:43:06 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,13 +14,12 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
-    
     def logged_in?
-      !!current_user
+      !!session[:user_id]
   	end
 
   	def current_user
-  		@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  		User.find(session[:user_id])
   	end
   end
 end
