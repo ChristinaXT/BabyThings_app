d9549627c1930fc46b358e98eac6a0831936bb70
Christina <40068707+ChristinaXT@users.noreply.github.com>
2019-03-18 19:41:45 -0400

    Update things_controller.rb

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -2,77 +2,79 @@ class ThingsController < ApplicationController
 
    get '/things' do
       if logged_in?
-        @user = current_user
         @things = Thing.all
         erb :'things/things'
       else
-        redirect "/login"
+        erb :'users/login', locals: {message: "You don't have access, please login"}
       end
     end
 
    
    get '/things/new' do
      if logged_in?
-          @user = current_user
-          erb :'things/new'
-        else
-          redirect "/login"
-        end
-    end
+        erb :'things/new'
+     else
+         erb :'users/login', locals: {message: "You don't have access, please login"}
+     end
+   end
    
     post '/things' do
-      	 @user = current_user
-      	
-      	if logged_in? && params[:content] != ""
-      	  @thing = current_user.things.build(content: params[:content])
-    	@thing.save
-			redirect "/things/#{@thing.id}"
-		elsif logged_in? && params[:content] == ""
-   		redirect '/things/new'
-		else
-   		redirect '/login'
- 		end
-	end
+       if params.values.any? {|value| value == ""}
+         erb :'projects/new', locals: {message: "Please try again"}
+       else   
+	  user = User.find(session[:user_id])
+      	  @thing = Thing.create(content: params[:content] user_id: user.id)
+    	  redirect "/things/#{@thing.id}"
+       end
+    end
     
-   
-   
       get '/things/:id' do
-        	if logged_in?
-         		@thing = Thing.find(params[:id])
-      			erb :'/things/show'
-         	else
-          	redirect '/login'
-        	end
+         if logged_in?
+         @thing = Thing.find(params[:id])
+      	   erb :'/things/show'
+         else
+          erb :'users/login', locals: {message: "Please try again"}
+        end
      end
 
-      post '/things/:id/edit' do
-    	     @thing = Thing.find(params[:id])
-    	     if logged_in? && @thing.user_id == current_user.id
-    	       erb :'/things/edit'
+      get '/things/:id/edit' do
+	 if logged_in?
+    	   @thing = Thing.find(params[:id])
+    	   if @thing.user_id == session[:user_id]
+    	      erb :'/things/edit'
    	   else
-            redirect '/login'
-  	     end
- 	   end
+              erb :'things', locals: {message: "You don't have access to edit list"}
+           end
+         else
+             erb :'users/login', locals: {message: "Please login"}
+         end
+      end
 
        patch '/things/:id' do
-    	    @thing = Thing.find(params[:id])
-  		     if logged_in? && params[:content] != ""
-             @thing.update(content: params[:content])
-      	      redirect "/things/#{@thing.id}"
-              else logged_in? && params[:content] == ""
-  		       redirect "/things/#{@thing.id}/edit"   
-     	     end
-   	  end
+    	  if params.values.any? {|value| value == ""}
+	     redirect to "/things/#{params[:id]}/edit"
+	  else
+            @thing = Thing.find(params[:id])
+      	    @thing.content = params[:content]
+            @thing.save
+            redirect to "/things/#{@thing.id}"
+          end
+       end		  
+	
+      # delete
+       delete '/things/:id/delete' do
+         @thing = Thing.find(params[:id])
+  	if session[:user_id]
+          @thing = Thing.find(params[:id])
+           if @thing.user_id == session[:user_id]
+              @thing.delete
+              redirect to '/things'
+           else
+              redirect to '/things'
+           end
+       else
+          redirect to '/login'
+      end
+   end
 
-         # delete
-       post '/things/:id/delete' do
-  		   @thing = Thing.find(params[:id])
-  		   @user = current_user
-  		   if logged_in? && @thing.user_id == @user.id
-  			   @thing.delete
-  	   	 erb :'/things/delete'
-  	     else
-  		  redirect '/login'
-  	end
-  end
 end
