b549a7b881670b2a1cef91a5dcbf16f5489557cd
Christina Williams <christinaxtwilliams@gmail.com>
2019-03-18 21:19:20 -0400

    updated migrations

--- db/migrate/20190315192341_create_things.rb
+++ db/migrate/20190315192341_create_things.rb
@@ -1,8 +1,8 @@
 class CreateThings < ActiveRecord::Migration[5.2]
   def change
-    create_table :things do |i|
-     i.string :content
-     i.integer :user_id
+    create_table :things do |t|
+     t.string :content
+     t.integer :user_id
    end
  end
 end
