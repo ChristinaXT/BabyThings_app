d783c18e8cec1a00d03bc4c686098f65ea563825
Christina Williams <christinaxtwilliams@gmail.com>
2019-04-01 17:47:41 +0000

    fixed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
   end
   
   get '/users/:id' do
-      @user = User.find(params[:id])
+      @user = current_user
+     # @user = User.find(params[:id])
       	erb :'/users/show'
     end
   end
