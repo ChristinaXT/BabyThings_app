2077e8ba2e1c89079057136be092c04bed2ea331
Guess <guess@Guesss-iMac.local>
2019-04-16 13:47:37 -0400

    cleaned up code

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,75 +1,73 @@
 class ThingsController < ApplicationController
 
- get '/things' do
+  get '/things' do
     if logged_in?
       @things = Thing.all
       erb :'/things/things'
     else
       redirect to "/login"
     end
- end
+  end
 
   get '/things/new' do
-     if logged_in?
-         erb :'/things/new'
-      else
-        redirect to "/login"
-   end
- end
-
-    post '/things' do
-      if params.values.any? {|value| value == ""}
-          redirect to "/things/new"
-        else
-      	  @thing = Thing.create(content: params[:content])
-      	  @thing.user_id = current_user.id
-      	  @thing.save
-          redirect to "/things/#{@thing.id}"
-       end
+    if logged_in?
+      erb :'/things/new'
+    else
+      redirect to "/login"
     end
+  end
 
-      get '/things/:id' do
-         if logged_in?
-           @thing = Thing.find(params[:id])
-      	  # @thing.user_id = current_user.id
-      	   erb :'/things/show'
-         else
-          redirect to "/login"
-        end
-     end
+  post '/things' do
+    if params.values.any? {|value| value == ""}
+      redirect to "/things/new"
+  else
+     @thing = Thing.create(content: params[:content])
+     @thing.user_id = current_user.id
+     @thing.save
+       redirect to "/things/#{@thing.id}"
+    end
+  end
 
-      get '/things/:id/edit' do
-    	    @thing = Thing.find(params[:id])
-    	     if @thing.user_id == session[:user_id]
-    	      erb :'/things/edit'
-   	     else
-             redirect to "/login"
-         end
+  get '/things/:id' do
+    if logged_in?
+      @thing = Thing.find(params[:id])
+    	erb :'/things/show'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/things/:id/edit' do
+     @thing = Thing.find(params[:id])
+    	 if @thing.user_id == session[:user_id]
+    	  erb :'/things/edit'
+   	  else
+        redirect to "/login"
       end
+  end
 
-       patch '/things/:id' do
-         @thing = Thing.find(params[:id])
-          if params.values.any? {|value| value == ""}
-	         redirect to "/things/#{params[:id]}/edit"
-	        else
-	         if @thing.user_id == current_user.id
-	           #@thing.update(params[:content])
-      	     @thing.content = params[:content]
-             @thing.save
-              redirect to "/things"
-          else
+   patch '/things/:id' do
+     @thing = Thing.find(params[:id])
+        if params.values.any? {|value| value == ""}
+	        redirect to "/things/#{params[:id]}/edit"
+	     else
+	       if @thing.user_id == current_user.id
+      	   @thing.content = params[:content]
+           @thing.save
             redirect to "/things"
-           end
-          end
-       end
+        else
+            redirect to "/things"
+        end
+      end
+   end
 
       # delete
-       delete '/things/:id/delete' do
-         @thing = Thing.find(params[:id])
-            if @thing.user_id == current_user.id
-              @thing.delete
-               erb :'/things/show'
-            end
-             redirect to "/things"
+    delete '/things/:id/delete' do
+      @thing = Thing.find(params[:id])
+        if @thing.user_id == current_user.id
+          @thing.delete
+           erb :'/things/show'
         end
+          redirect to "/things"
+      end
    end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,51 @@
 class UsersController < ApplicationController
 
 
-   get '/signup' do
-       if !logged_in?
-        erb :'/users/create_user'
-      else
-        redirect to "/things"
-     end
+  get '/signup' do
+    if !logged_in?
+    erb :'/users/create_user'
+   else
+    redirect to "/things"
     end
-  
+  end
+
   post '/signup' do
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    if params.values.any? {|value| value == ""}
+      if params.values.any? {|value| value == ""}
       erb :'/users/create_user'
     elsif User.username_check(@user.username)
       redirect to "/signup"
-    else
-      @user.save
-      session[:user_id] = @user.id
-      redirect to "/users/#{@user.id}"
-    end
+     else
+       @user.save
+       session[:user_id] = @user.id
+        redirect to "/users/#{@user.id}"
+     end
   end
-  
+
   get '/users/:id' do
     if logged_in?
       @user = current_user
       @things = current_user.things
-      	erb :'/users/show'
-    else 
-      redirect to "/login"
+      erb :'/users/show'
+     else
+       redirect to "/login"
     end
   end
-  
+
   get '/login' do
     if !logged_in?
-        erb :'/users/login'
-      else
-        redirect to "/things"
-      end
+     erb :'/users/login'
+    else
+      redirect to "/things"
     end
+  end
 
   post '/login' do
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect to "/things"
-    else
+  else
       redirect to "/login"
     end
   end
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
     if session[:user_id] != nil
       session.destroy
       redirect to "/"
-    else
+  else
       redirect to "/things"
     end
   end
